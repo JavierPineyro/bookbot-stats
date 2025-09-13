bc731c6e0dd1fb10ee6d91c230daec47afd64dd2
Javier Piñeyro <dariojavierpineyro@gmail.com>
2025-09-12 23:24:15 -0300

    add libs folder

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from libs import count_words
 
 def main():
-    print("Esto es el proyectoo vamooo")
+    path_to_book = "./books/frankenstein.txt"
+    with open(path_to_book) as f:
+        contents = f.read()
+    num_of_words: int = count_words(contents)
+    print(f"{num_of_words} words found in the doc")
 
 if(__name__ == '__main__'):
     main()
